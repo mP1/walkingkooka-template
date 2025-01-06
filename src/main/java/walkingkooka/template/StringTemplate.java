e26ec48ac661fe90567a09a5d960dceb6450b9f2
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-06 19:31:02 +1100

    StringTemplate

---
+++ src/main/java/walkingkooka/template/StringTemplate.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.template;
+
+import walkingkooka.text.printer.Printer;
+
+import java.util.Objects;
+
+/**
+ * A {@link Template} that renders the text given to its factory.
+ */
+final class StringTemplate implements Template {
+
+    /**
+     * Empty singleton.
+     */
+    // @VisibleForTesting
+    final static StringTemplate EMPTY = new StringTemplate("");
+
+    static StringTemplate with(final String text) {
+        Objects.requireNonNull(text, "text");
+
+        return text.isEmpty() ?
+                EMPTY :
+                new StringTemplate(text);
+    }
+
+    private StringTemplate(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void render(final Printer printer,
+                       final TemplateContext context) {
+        printer.print(this.text);
+    }
+
+    private final String text;
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+}
---
+++ src/test/java/walkingkooka/template/StringTemplateTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.template;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class StringTemplateTest implements TemplateTesting,
+        ToStringTesting<StringTemplate>,
+        ClassTesting<StringTemplate> {
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> StringTemplate.with(null)
+        );
+    }
+
+    @Test
+    public void testWithEmptyString() {
+        assertSame(
+                StringTemplate.EMPTY,
+                StringTemplate.with("")
+        );
+    }
+
+    // render...........................................................................................................
+
+    @Test
+    public void testRender() {
+        final String text = "Hello123";
+
+        this.renderAndCheck(
+                StringTemplate.with(text),
+                TemplateContexts.fake(),
+                text
+        );
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                StringTemplate.with("Hello"),
+                "Hello"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<StringTemplate> type() {
+        return StringTemplate.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
