0838c0bac2612c40b12e6b2565802349111f7f15
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-08 11:23:20 +1100

    it-tests testParseAndRender

--- src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -1,10 +1,32 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package test;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
-import walkingkooka.j2cl.locale.LocaleAware;
+import walkingkooka.template.TemplateContext;
+import walkingkooka.template.TemplateContexts;
+import walkingkooka.text.LineEnding;
+import walkingkooka.text.cursor.TextCursors;
+import walkingkooka.text.printer.Printers;
 
-@LocaleAware
+@walkingkooka.j2cl.locale.LocaleAware
+
 public class TestGwtTest extends GWTTestCase {
 
     @Override
@@ -16,6 +38,35 @@ public class TestGwtTest extends GWTTestCase {
         assertEquals(
                 1,
                 1
+        );
+    }
+
+    public void testParseAndRender() {
+        final TemplateContext context = TemplateContexts.expressionTemplateValueName(
+                (n) -> "<<" + n.text().toUpperCase() + ">>"
+        );
+
+        final StringBuilder b = new StringBuilder();
+
+        context.parseAndRender(
+                TextCursors.charSequence("Hello ${abc} 123"),
+                Printers.stringBuilder(
+                        b,
+                        LineEnding.NL
+                )
+        );
+
+        checkEquals(
+                "Hello <<ABC>> 123",
+                b.toString()
+        );
+    }
+
+    private void checkEquals(final String expected,
+                             final String actual) {
+        assertEquals(
+                expected,
+                actual
         );
     }
 }
